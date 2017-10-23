717444153ecde11b01bd33dd04efafd44abd830c
NIKHILESH <naga.nikhil65@gmail.com>
2017-10-24 00:10:52 +0530

    c

--- GetION/PresentationLayer/CustomViews/AddCustomPopUpView.swift
+++ GetION/PresentationLayer/CustomViews/AddCustomPopUpView.swift
@@ -12,9 +12,10 @@ class AddCustomPopUpView: UIView,UIScrollViewDelegate {
     @IBOutlet var btnClose: UIButton!
     @IBOutlet var scrlVw: UIScrollView!
     @IBOutlet weak var btnSelPromo: UIButton!
-    
+    @IBOutlet weak var vwBase: UIView!
     @IBOutlet var constVwBgWidth: NSLayoutConstraint!
     
+    @IBOutlet weak var imgVwBase: UIImageView!
     func designScreen(screenWidth : CGFloat)
     {
         scrlVw.contentSize = CGSize (width: (screenWidth - 40) * 3  , height: 0)
@@ -23,6 +24,10 @@ class AddCustomPopUpView: UIView,UIScrollViewDelegate {
         
         btnSelPromo.layer.cornerRadius = 10.0
         btnSelPromo.layer.masksToBounds = true
+     
+        imgVwBase.layer.cornerRadius = 10.0
+        imgVwBase.layer.masksToBounds = true
+
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
