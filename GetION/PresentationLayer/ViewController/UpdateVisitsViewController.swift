879efa79db65b07ab9f845710e94673134df4793
Selvam <selvam.k@capillarytech.com>
2017-10-27 22:04:31 +0530

    updatevisite changes

--- GetION/PresentationLayer/ViewController/UpdateVisitsViewController.swift
+++ GetION/PresentationLayer/ViewController/UpdateVisitsViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class UpdateVisitsViewController: UIViewController {
-
+class UpdateVisitsViewController: BaseViewController
+{
+    @IBOutlet weak var vwScrollMain: UIScrollView!
+    
+    @IBOutlet weak var vwMain: UIView!
+    @IBOutlet weak var constrtVwMainHeight: NSLayoutConstraint!
+    @IBOutlet weak var imgProfile: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var vwAppointmentDetails: UIView!
+    @IBOutlet weak var lblAppointmentDate: UILabel!
+    @IBOutlet weak var lblAppointmentTime: UILabel!
+    @IBOutlet weak var lblDocName: UILabel!
+    @IBOutlet weak var btnAccept: UIButton!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtAmountDue: UITextField!
+    @IBOutlet weak var txtDOB: UITextField!
+    @IBOutlet weak var txtGender: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtAreaLocality: UITextField!
+    @IBOutlet weak var txtSource: UITextField!
+    @IBOutlet weak var txtVwRemarks: UITextView!
+    
+    
+    
+    
+    var objVisits = VisitsBO()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.designNavigationBar()
+        designTabBar()
+        setSelectedButtonAtIndex(3)
+        
+        self.vwAppointmentDetails.layer.borderColor = UIColor.lightGray.cgColor
+        self.vwAppointmentDetails.layer.borderWidth = 0.8
         // Do any additional setup after loading the view.
     }
 
