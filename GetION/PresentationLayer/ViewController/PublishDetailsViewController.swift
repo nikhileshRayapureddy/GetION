a0dcf2662cdf9a749d3d4387d895a5cd3ab77512
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2017-11-01 00:05:52 +0530

    commit publish details

---
+++ GetION/PresentationLayer/ViewController/PublishDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PublishDetailsViewController.swift
+//  GetION
+//
+//  Created by Kiran Kumar on 31/10/17.
+//  Copyright © 2017 Nikhilesh. All rights reserved.
+//
+
+import UIKit
+
+class PublishDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
