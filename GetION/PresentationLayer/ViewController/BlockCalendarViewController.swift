7d2a33045cad91b480c6c2484ec28977dab36e74
Nikhilesh <nikhilesh.rayapureddy@capillarytech.com>
2017-11-04 12:00:03 +0530

    Commit

--- GetION/PresentationLayer/ViewController/BlockCalendarViewController.swift
+++ GetION/PresentationLayer/ViewController/BlockCalendarViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class BlockCalendarViewController: UIViewController {
+class BlockCalendarViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.designNavigationBar()
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +20,4 @@ class BlockCalendarViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
